f0b3a9f85be1233c2c0be878e62a657d910471dd
Sven Nierlein <Sven.Nierlein@consol.de>
2015-05-15 16:03:15 +0200

    thruk: prepare next update

--- packages/thruk/skel/etc/thruk/fcgid_env.sh
+++ packages/thruk/skel/etc/thruk/fcgid_env.sh
@@ -4,7 +4,7 @@
 [ -e ###ROOT###/.thruk   ] && . ###ROOT###/.thruk
 
 # set omd environment
-export CATALYST_CONFIG="###ROOT###/etc/thruk"
+export THRUK_CONFIG="###ROOT###/etc/thruk"
 
 # execute fastcgi server
 exec "###ROOT###/share/thruk/script/thruk_fastcgi.pl";
